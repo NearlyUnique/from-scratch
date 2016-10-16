8abca204e5c05dd36375cd6f4d5da8cd9bb28bd6
Adam Straughan <adam.straughan@gmail.com>
2016-10-16 21:19:40 +0100

    loop 3 times

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	t0 := time.Now()
+	rand.Seed(t0.Unix())
+
+	for i := 0; i < 3; i++ {
+		delay := rand.Intn(500)
+		time.Sleep(time.Duration(delay) * time.Millisecond)
+		fmt.Printf("Collecting %d ...\n", i)
+	}
+
+	fmt.Printf("Done in %v\n", time.Since(t0))
 }
